bce9d6b77f024c71742e98e0e4395d00ec2be677
Ngai Lam Ho <nxh912@gmail.com>
2026-06-11 23:47:39 +0800

    table format

--- src/views/mro_coder/MRO_Control.vue
+++ src/views/mro_coder/MRO_Control.vue
@@ -96,6 +96,65 @@ const downloadCSV = () => {
           Save as CSV
         </button>
 
+        <CTable striped>
+          <CTableHead>
+            <CTableRow>
+              <CTableHeaderCell scope="col">#</CTableHeaderCell>
+              <CTableHeaderCell scope="col">Section</CTableHeaderCell>
+              <CTableHeaderCell scope="col">Name</CTableHeaderCell>
+              <!-- <CTableHeaderCell scope="col">Heading</CTableHeaderCell>-->
+            </CTableRow>
+          </CTableHead>
+          <CTableBody>
+            <CTableRow>
+              <CTableHeaderCell scope="row">1</CTableHeaderCell>
+              <CTableDataCell>Mark</CTableDataCell>
+              <CTableDataCell>Otto</CTableDataCell>
+              <!--
+              <CTableDataCell>@mdo</CTableDataCell>
+              -->
+            </CTableRow>
+            <CTableRow>
+              <CTableHeaderCell colSpan={4}>
+                <CTable>
+                  <CTableHead>
+                    <CTableRow>
+                      <CTableHeaderCell scope="col">Section</CTableHeaderCell>
+                      <CTableHeaderCell scope="col">Field</CTableHeaderCell>
+                      <CTableHeaderCell scope="col">Detail</CTableHeaderCell>
+                    </CTableRow>
+                  </CTableHead>
+                  <CTableBody>
+                    <CTableRow>
+                      <CTableHeaderCell scope="row">A</CTableHeaderCell>
+                      <CTableDataCell>First</CTableDataCell>
+                      <CTableDataCell>Last</CTableDataCell>
+                    </CTableRow>
+                    <CTableRow>
+                      <CTableHeaderCell scope="row">B</CTableHeaderCell>
+                      <CTableDataCell>First</CTableDataCell>
+                      <CTableDataCell>Last</CTableDataCell>
+                    </CTableRow>
+                    <!--
+                    <CTableRow>
+                      <CTableHeaderCell scope="row">C</CTableHeaderCell>
+                      <CTableDataCell>First</CTableDataCell>
+                      <CTableDataCell>Last</CTableDataCell>
+                    </CTableRow>
+                    -->
+                  </CTableBody>
+                </CTable>
+              </CTableHeaderCell>
+            </CTableRow>
+            <CTableRow>
+              <CTableHeaderCell scope="row">2</CTableHeaderCell>
+              <CTableDataCell colSpan={2}>Larry the Bird</CTableDataCell>
+              <CTableDataCell>@twitter</CTableDataCell>
+            </CTableRow>
+          </CTableBody>
+        </CTable>
+
+        <!--
         <table class="table table-sm">
           <thead>
             <tr>
@@ -125,7 +184,7 @@ const downloadCSV = () => {
             </tr>
           </tbody>
         </table>
-
+        -->
     
       </CCard>
     </CCol>
